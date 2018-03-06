526af0aa3f5145144187582b04a67c0810984fb2
Hanaa Mohamed <hanaa.ghobashi@symbyo.com>
2018-03-06 13:59:33 +0200

    introduce benchmarking

--- scratch/main.cpp
+++ scratch/main.cpp
@@ -1,48 +1,10 @@
+#include "benchmark.h"
 
-#include <entt/entity/registry.hpp>
-#include <cpprelude/defines.h>
-#include <cpprelude/string.h>
-#include <cpprelude/dynamic_array.h>
-
-#include <ecs/world.h>
-#include <ecs/helper_structures/view.h>
-
-
-using namespace ecs;
-using namespace cpprelude;
-using namespace utility;
-
-
-struct Position
-{
-	r32 x = 0.0f;
-	r32 y = 0.0f;
-	r32 z = 0.0f;
-
-	Position()
-	{}
-
-	Position(r32 i, r32 j, r32 k)
-		:x(i), y(j), z(k)
-	{}
-};
-
-struct Implant
-{
-	cpprelude::u64 id = INVALID_ID;
-	World* world = nullptr;
-};
 
 int main()
 {
-	World w;
+	/*ecs::World w;
 	auto e = w.create_entity();
-	auto e1 = w.create_entity();
-
-	for(cpprelude::u64 i = 0; i < 100; ++i)
-	{ 
-		auto e = w.create_entity();
-		w.add_property<u32>(e, 838);
-	}
-	//w.remove_property<r32>(e);
+	w.add_property<Position>(e);*/
+	benchmark();
 }
